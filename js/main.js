1a9784f72ad1358bd493e2d73b515183e7afab7d
Dmitry Bogutskii <bogutskii.d@gmail.com>
2019-05-24 15:29:54 +0300

    finally did js13

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+
+document.getElementById('btn').addEventListener('click', function () {
+    localStorage.setItem('mode', (localStorage.getItem('mode') || 'dark') === 'dark' ? 'light' : 'dark');
+    localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+    ((localStorage.getItem('mode') || 'dark') === 'dark') ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+});
+
+
